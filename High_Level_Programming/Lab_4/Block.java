abcf5215147f2386014ee424a204f1c3492e47c1
pitt0n <matweew20006@gmail.com>
2025-11-15 18:05:21 +0300

    Написал класс Block

--- High_Level_Programming/Lab_4/Block.java
+++ High_Level_Programming/Lab_4/Block.java
@@ -1,12 +1,30 @@
 package High_Level_Programming.Lab_4;
 
 class Block extends Item{
-    private boolean poln;
+    private boolean full;
     
 
-    public Block(String name, String id, int maxStackSize, boolean poln){
+    public Block(String name, String id, int maxStackSize, int quantitym, boolean full){
+        super(name, id, maxStackSize, quantitym);
+        this.full = full;
+    }
+
+    public Block(String name, String id, int maxStackSize, boolean full){
         super(name, id, maxStackSize);
-            this.poln = poln;
+        this.full = full;
+    }
+
+    public Block(String name, String id, boolean full){
+        super(name, id);
+        this.full = full;
     }
     
+    public void setFull(boolean full){this.full = full;}
+    public boolean getFull(){return full;}
+
+    public void getInfo(){
+        System.out.printf("Название предмета: %s\nID предмета: %s\nМаксимальное количество в слоте: %d\nКоличество предметов в яцейке: %d\n" + //
+                        "Полный блок: %b\n", getName(), getId(), getMaxStackSize(), getQuantity(), full);
+    }
+
 }
