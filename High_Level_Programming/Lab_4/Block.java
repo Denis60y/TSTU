513971b0aa6f61d8e7f0ba88c9c3a8f1f6a24527
pitt0n <matweew20006@gmail.com>
2025-11-16 21:51:55 +0300

    Добавление новых полей и функции use

--- High_Level_Programming/Lab_4/Block.java
+++ High_Level_Programming/Lab_4/Block.java
@@ -2,22 +2,33 @@ package High_Level_Programming.Lab_4;
 
 class Block extends Item {
     private boolean full;
+    private boolean fall;
+    private boolean transparency;
 
-    public Block(String name, String id, int maxStackSize, int quantity, boolean full) {
+    // Конструкторы
+    public Block(String name, String id, int maxStackSize, int quantity, boolean full, boolean fall,
+            boolean transparency) {
         super(name, id, maxStackSize, quantity);
         this.full = full;
+        this.fall = fall;
+        this.transparency = transparency;
     }
 
-    public Block(String name, String id, int maxStackSize, boolean full) {
+    public Block(String name, String id, int maxStackSize, boolean full, boolean fall, boolean transparency) {
         super(name, id, maxStackSize);
         this.full = full;
+        this.fall = fall;
+        this.transparency = transparency;
     }
 
-    public Block(String name, String id, boolean full) {
+    public Block(String name, String id, boolean full, boolean fall, boolean transparency) {
         super(name, id);
         this.full = full;
+        this.fall = fall;
+        this.transparency = transparency;
     }
 
+    // getter'ы и setter'ы
     public void setFull(boolean full) {
         this.full = full;
     }
@@ -26,12 +37,32 @@ class Block extends Item {
         return full;
     }
 
+    public void setFall(boolean fall) {
+        this.fall = fall;
+    }
+
+    public boolean getFall() {
+        return fall;
+    }
+
+    public void setTransparency(boolean transparency) {
+        this.transparency = transparency;
+    }
+
+    public boolean getTransparency() {
+        return transparency;
+    }
+
+    // Функция тотображения всей информации
     public void getInfo() {
         System.out.printf(
                 "Название предмета: %s\nID предмета: %s\nМаксимальное количество в слоте: %d\nКоличество предметов в яцейке: %d\n"
-                        + //
-                        "Полный блок: %b\n",
-                getName(), getId(), getMaxStackSize(), getQuantity(), full);
+                        + "Полный блок: %b\nПадающий блок: %b\nПрозрачный блок: %b\n",
+                getName(), getId(), getMaxStackSize(), getQuantity(), full, fall, transparency);
     }
 
+    // Функция использования предмета
+    public void use() {
+        setQuantity(getQuantity() - 1);
+    }
 }
