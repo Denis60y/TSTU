154860ebd276b6a3c97b0909983f72a903ca5d9c
Pitt0n <matweew20006@gmail.com>
2025-11-19 15:05:51 +0300

    Проверка

--- High_Level_Programming/Lab_4/Task1.java
+++ High_Level_Programming/Lab_4/Task1.java
@@ -6,15 +6,20 @@ package High_Level_Programming.Lab_4;
  * 
  * Автор: @Pitt0n
  * Дата создания: 08.11.2025
- * Версия: 0.0.7
+ * Версия: 0.0.8
  */
 
 
 public class Task1 {
     public static void main(String[] args) {
-        Food item1 = new Food("Яблоко", "apple", 64, 1, 5, 35);
-        Player Steve = new Player(20, 10, false);
-        Block block = new Block("Grass", "grass", true, false, false);
-        block.getInfo();
+        // Food item1 = new Food("Яблоко", "apple", 64, 1, 5, 35);
+        // Player Steve = new Player(20, 10, false);
+        // Block block = new Block("Grass", "grass", true, false, false);
+        // Inventory inventory = new Inventory();
+        // inventory.addItem(item1);
+        // inventory.addItem(block);
+        // inventory.getItems();
+        // inventory.getInfo();
+        System.out.println("pop");
     }
 }
