406031a34f1f90a4d88379112610b0788d638b06
Pitt0n <matweew20006@gmail.com>
2025-11-19 21:55:35 +0300

    Новые фукнции и реализация хранения предметов в инвентаре

--- High_Level_Programming/Lab_4/Player.java
+++ High_Level_Programming/Lab_4/Player.java
@@ -1,14 +1,35 @@
 package High_Level_Programming.Lab_4;
 
 class Player {
+    private String name;
     private int HP;
     private int satiety;
     private boolean buff;
+    private Item[] items;
+    private final int maxSize = 10;
+    private int cursor = 0;
 
-    public Player(int HP, int satiety, boolean buff) {
+    public Player(String name, int HP, int satiety, boolean buff) {
+        this.name = name;
         this.HP = HP;
         this.satiety = satiety;
         this.buff = buff;
+        this.items = new Item[this.maxSize];
+    }
+
+
+
+
+
+
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public void setHP(int HP) {
@@ -20,7 +41,7 @@ class Player {
     }
 
     public void setSatiety(int satiety) {
-        this.satiety = satiety;
+        this.satiety = satiety; 
     }
 
     public int getSatiety() {
@@ -35,9 +56,57 @@ class Player {
         return buff;
     }
 
+
+
+    public void setItem(Item item, int number){
+        this.items[number] = item;
+    }
+
+    public Item[] getItems(){
+        return items;
+    }
+
+    public void addItem(Item item) {
+        if (cursor < maxSize) {
+            this.items[cursor] = item;
+            item.setIndex(cursor);
+            cursor++;
+        } else {
+            System.out.println("Инвентарь полон!");
+        }
+    }
+
+    public void getInfoItems(){
+        for (int i = 0; i < maxSize; i++){
+            if (items[i] != null){
+                items[i].getInfo();
+                System.out.println("\n");
+            }
+            else{
+                continue;
+            }
+        }
+    }
+
+
+
+
+
+
+
+
     public void getInfo() {
         System.out.printf("HP персонажа: %d\nСытость персонажа: %d\nНаличие эффекта: %b\n", HP, satiety, buff);
     }
+
+
+
+
+
+
+
+
+
 
     public void drinkMilk() {
         this.buff = false;
@@ -48,6 +117,7 @@ class Player {
             food.eaten();
             this.HP += food.getSaturationLevel();
             this.satiety += food.getFoodLevel();
+            this.buff = food.getEnchanted();
         } else {
             System.out.println("Не хватает предмета");
         }
