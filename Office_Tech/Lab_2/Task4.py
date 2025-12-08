d01a544d5b0b888b86b15dbdee25becaab580ac3
Pitt0n <matweew20006@gmail.com>
2025-12-08 06:30:02 +0300

    Заполнение с клавиатуры (не готово)

--- Office_Tech/Lab_2/Task4.py
+++ Office_Tech/Lab_2/Task4.py
@@ -3,6 +3,43 @@ import docx.enum.text
 from docx.shared import Cm, Mm, Pt
 from docx.enum.text import WD_LINE_SPACING
 
+personal = []
+photos = []
+text_photo = []
+
+personal.append(input("Полное название организации: "))
+
+personal.append(input("Название кафедры: "))
+
+personal.append(input("№ лабораторной: "))
+
+personal.append(input("Название дисциплины: "))
+
+personal.append(input("Номер группы: "))
+
+personal.append(input("Фамилия И.О. студента: "))
+
+personal.append(input("Фамилия И.О. преподавателя: "))
+
+personal.append(input("Год: "))
+
+personal.append(input("Цель работы: "))
+
+personal.append(input("Вариант лабораторной: "))
+
+personal.append(input("Задание: "))
+
+personal.append(input("Решение: "))
+
+personal.append(input("Код программы: "))
+
+for i in range(int(input("Количество картинок: "))):
+    photos.append(input(f"Путь к картинке номмер{i+1}: "))
+    text_photo.append(f"Рисунок {i+1}. {input("Подписть к картинке: ")}")
+
+personal.append(input("Выводы: "))
+
+
 doc = docx.Document()
 
 style = doc.styles['Normal']
@@ -21,7 +58,7 @@ text = doc.add_paragraph()
 p_fmt = text.paragraph_format
 p_fmt.left_indent = Cm(-0.63)
 p_fmt.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
-add = text.add_run("Полное название организации")
+add = text.add_run(f"{personal[0]}")
 add.font.size = Pt(14)
 
 text = doc.add_paragraph()
@@ -34,7 +71,7 @@ text = doc.add_paragraph()
 p_fmt = text.paragraph_format
 p_fmt.left_indent = Cm(-0.63)
 p_fmt.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.RIGHT
-add = text.add_run("Название кафедры")
+add = text.add_run(f"{personal[1]}")
 add.font.size = Pt(14)
 
 text = doc.add_paragraph()
@@ -47,14 +84,14 @@ text = doc.add_paragraph()
 p_fmt = text.paragraph_format
 p_fmt.left_indent = Cm(-0.63)
 p_fmt.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
-add = text.add_run("Отчет по лабораторной работе №#")
+add = text.add_run(f"Отчет по лабораторной работе №{personal[2]}")
 add.font.size = Pt(24)
 
 text = doc.add_paragraph()
 p_fmt = text.paragraph_format
 p_fmt.left_indent = Cm(-0.63)
 p_fmt.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
-add = text.add_run("по дисциплине «Название дисциплины»")
+add = text.add_run(f"по дисциплине «{personal[3]}»")
 add.font.size = Pt(16)
 
 text = doc.add_paragraph()
@@ -72,19 +109,19 @@ p_fmt = text.paragraph_format
 p_fmt.left_indent = Cm(-0.63)
 add = text.add_run("                                                           ")
 add.font.size = Pt(18)
-add = text.add_run("Выполнил: студент группы Номер ")
+add = text.add_run(f"Выполнил: студент группы {personal[4]}")
 add.font.size = Pt(14)
 
 text = doc.add_paragraph()
 p_fmt = text.paragraph_format
 p_fmt.left_indent = Cm(-0.63)
-add = text.add_run("                                                                                                Фамилия И.О.")
+add = text.add_run(f"                                                                                                {personal[5]}")
 add.font.size = Pt(14)
 
 text = doc.add_paragraph()
 p_fmt = text.paragraph_format
 p_fmt.left_indent = Cm(-0.63)
-add = text.add_run("                                                                            Проверил:  Фамилия И.О.")
+add = text.add_run(f"                                                                            Проверил:  {personal[6]}")
 add.font.size = Pt(14)
 
 text = doc.add_paragraph()
@@ -98,7 +135,7 @@ add.font.size = Pt(14)
 text = doc.add_paragraph()
 p_fmt = text.paragraph_format
 p_fmt.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
-add = text.add_run("Тамбов 2001")
+add = text.add_run(f"Тамбов {personal[7]}")
 add.font.size = Pt(14)
 
 text = doc.add_paragraph()
@@ -112,7 +149,7 @@ add.bold = True
 text = doc.add_paragraph()
 p_fmt = text.paragraph_format
 p_fmt.first_line_indent = Cm(1.25)
-add = text.add_run("Зачем именно нужна данная работа. Что она позволяет освоить\n")
+add = text.add_run(f"{personal[8]}\n")
 add.font.size = Pt(14)
 
 text = doc.add_paragraph()
@@ -126,13 +163,13 @@ add.bold = True
 text = doc.add_paragraph()
 p_fmt = text.paragraph_format
 p_fmt.first_line_indent = Cm(1.25)
-add = text.add_run("Вариант №n")
+add = text.add_run(f"Вариант №{personal[9]}")
 add.font.size = Pt(14)
 
 text = doc.add_paragraph()
 p_fmt = text.paragraph_format
 p_fmt.first_line_indent = Cm(1.25)
-add = text.add_run("Текст задания\n")
+add = text.add_run(f"{personal[10]}\n")
 add.font.size = Pt(14)
 
 text = doc.add_paragraph()
@@ -151,7 +188,7 @@ add.bold = True
 text = doc.add_paragraph()
 p_fmt = text.paragraph_format
 p_fmt.first_line_indent = Cm(0.95)
-add = text.add_run("Каким способом решалась указанная выше задача. Что использовалось для её выполнения.\n")
+add = text.add_run(f"{personal[11]}\n")
 add.font.size = Pt(14)
 
 text = doc.add_paragraph()
@@ -168,7 +205,7 @@ add.font.name = "Courier New"
 
 text = doc.add_paragraph()
 p_fmt = text.paragraph_format
-add = text.add_run("Код программы")
+add = text.add_run(f"{personal[12]}")
 add.font.size = Pt(12)
 add.font.name = "Consolas"
 
@@ -183,29 +220,43 @@ add = text.add_run("Результаты работы программы")
 add.font.size = Pt(14)
 add.bold = True
 
-text = doc.add_paragraph()
-p_fmt = text.paragraph_format
-p_fmt.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
-add = text.add_run("КАРТИНКА")
-add.font.size = Pt(12)
+# text = doc.add_paragraph()
+# p_fmt = text.paragraph_format
+# p_fmt.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
+# add = text.add_run("КАРТИНКА")
+# add.font.size = Pt(12)
 
-text = doc.add_paragraph()
-p_fmt = text.paragraph_format
-p_fmt.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
-add = text.add_run("Рисунок 1. Подпись к рисунку")
-add.font.size = Pt(12)
+# text = doc.add_paragraph()
+# p_fmt = text.paragraph_format
+# p_fmt.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
+# add = text.add_run("Рисунок 1. Подпись к рисунку")
+# add.font.size = Pt(12)
 
-text = doc.add_paragraph()
-p_fmt = text.paragraph_format
-p_fmt.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
-add = text.add_run("КАРТИНКА")
-add.font.size = Pt(12)
+# text = doc.add_paragraph()
+# p_fmt = text.paragraph_format
+# p_fmt.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
+# add = text.add_run("КАРТИНКА")
+# add.font.size = Pt(12)
 
-text = doc.add_paragraph()
-p_fmt = text.paragraph_format
-p_fmt.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
-add = text.add_run("Рисунок 2. Подпись к рисунку")
-add.font.size = Pt(12)
+# text = doc.add_paragraph()
+# p_fmt = text.paragraph_format
+# p_fmt.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
+# add = text.add_run("Рисунок 2. Подпись к рисунку")
+# add.font.size = Pt(12)
+
+
+for i in range(len(photos)):
+    text = doc.add_paragraph()
+    p_fmt = text.paragraph_format
+    p_fmt.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
+    add = text.add_run(f"{photos[i]}")
+    add.font.size = Pt(12)
+
+    text = doc.add_paragraph()
+    p_fmt = text.paragraph_format
+    p_fmt.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
+    add = text.add_run(f"{text_photo[i]}")
+    add.font.size = Pt(12)
 
 text = doc.add_paragraph()
 p_fmt = text.paragraph_format
@@ -237,7 +288,7 @@ add.bold = True
 text = doc.add_paragraph()
 p_fmt = text.paragraph_format
 p_fmt.first_line_indent = Cm(0.95)
-add = text.add_run("К каким выводам вы пришли при решении данной задачи. Что освоили, что сделали, чего достигли.")
+add = text.add_run(f"{personal[13]}")
 add.font.size = Pt(14)
 
 doc.save("resources/Структура отчета.docx")
